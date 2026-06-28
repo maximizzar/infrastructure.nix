bf1da11809428cf2021930b192f0d5ae5a781470
maximizzar <mail@maximizzar.de>
2026-06-28 23:36:39 +0200

    feat: minimal working runner

---
+++ hosts/runner/configuration.nix
@@ -0,0 +1,32 @@
+{
+  modulesPath,
+  lib,
+  stateVersion,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  # Hardware configuration
+  maximizzar.modules.hardware.bootSystemd.enable = lib.mkDefault true;
+  maximizzar.modules.hardware.configuration.qemu.enable = lib.mkDefault true;
+  maximizzar.modules.hardware.rootDisk.enable = lib.mkDefault true;
+
+  # Core Services
+  services.openssh.enable = lib.mkDefault true;
+  services.qemuGuest.enable = lib.mkDefault true;
+
+  # Networking
+  networking.hostName = "runner";
+  maximizzar.networking.ipForwarding.enable = true;
+  maximizzar.networking.vmWanInterface.enable = true;
+
+  # User Settings
+  security.sudo.wheelNeedsPassword = lib.mkDefault false;
+  maximizzar.modules.users.maximizzar.enable = lib.mkDefault true;
+
+  system.stateVersion = stateVersion;
+}
---
+++ hosts/runner/default.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }: {
+  imports = [
+    ./configuration.nix
+  ];
+
+  systemd.services.forgejo-runner = {
+    description = "forgejo-runner";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    path = with pkgs; [
+      bash
+      coreutils
+      git
+      nodejs
+      pkgs.forgejo-runner
+    ];
+
+    environment = {
+      FORGEJO_INSTANCE_URL = "https://forgejo.maximizzar.io";
+    };
+
+    serviceConfig = {
+      Type = "simple";
+
+      # Wir erzwingen ROOT, um jede Art von Berechtigungsproblem auszuschließen
+      User = "root";
+      Group = "root";
+
+      # Das Arbeitsverzeichnis, in dem die Registrierungsdaten (.runner) landen
+      WorkingDirectory = "/var/lib/forgejo-runner";
+
+      # Lädt dein Token direkt aus der Datei als Umgebungsvariable $TOKEN
+      EnvironmentFile = "/var/secrets/forgejo-runner/token";
+
+      # Der eigentliche Startbefehl:
+      # Erst registrieren (falls nötig), dann den Daemon starten.
+      #ExecStartPre = "${pkgs.forgejo-runner}/bin/forgejo-runner register --no-interactive --instance $FORGEJO_INSTANCE_URL --token $TOKEN --name nixos-manual-runner --labels self-hosted:host,nixos:host";
+      ExecStart = "${pkgs.forgejo-runner}/bin/forgejo-runner daemon --url https://forgejo.maximizzar.io/ --uuid b994f831-d0d1-49c8-a5ec-19a7f6b7606c --token-url file:///var/secrets/forgejo-runner/token --label nixos:host";
+
+      Restart = "always";
+      RestartSec = "5s";
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/forgejo-runner 0700 root root - -"
+  ];
+}
