115729c78cca2c29e823bbc6bd8d67deb7f44c69
maximizzar <mail@maximizzar.de>
2026-09-03 18:18:14 +0200

    chore: update repository config

--- flake.nix
+++ flake.nix
@@ -1,62 +1,148 @@
 # SPDX-FileCopyrightText: 2026 maximizzar <mail@maximizzar.de>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
 {
   description = "NixOS configuration for my Homelab!";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
 
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Secrets-Management
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
-  outputs = { self, nixpkgs, disko, sops-nix, ... }@inputs: let
-    system = "x86_64-linux";
-    lib = nixpkgs.lib;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-facter-modules,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      lib = nixpkgs.lib;
+      inventory = import ./inventory;
+      sources = nixpkgs.legacyPackages.${system}.callPackage ./_sources/generated.nix { };
+      modules = [
+        ./modules/default-os.nix
+        inputs.sops-nix.nixosModules.sops
+        inputs.disko.nixosModules.disko
+      ];
 
-    baseModules = [
-        ./modules/services/common/sshd.nix
-        ./modules/services/common/pki.nix
-        ./modules/users/default.nix
-    ];
-
-  in {
-    # Host Configurations (for deployment)
-    nixosConfigurations = {
-      generic-guest = lib.nixosSystem {
-        inherit system;
-        modules = baseModules ++ [ ({modulesPath, ...}: {
-          imports = [ "${modulesPath}/profiles/qcow2.nix" ];
-          system.stateVersion = "25.11";
-        }) ];
-      };
-
-      nameserver = lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
-
-        modules = [
-          ./hosts/nameserver/default.nix
-          disko.nixosModules.disko
-          sops-nix.nixosModules.sops
+      release-script = pkgs.writeShellApplication {
+        name = "release";
+        runtimeInputs = [
+          pkgs.goreleaser
+          pkgs.git
+          pkgs.bash
         ];
+        text = builtins.readFile ./scripts/release.sh;
+      };
+    in
+    {
+
+      nixosConfigurations = {
+        gw-nbg = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/gw-nbg ];
+        };
+
+        gw-genesis = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/gw-genesis ];
+        };
+
+        resolver = lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit
+              inputs
+              inventory
+              sources
+              ;
+          };
+          modules = modules ++ [ ./hosts/resolver ];
+        };
+
+        prometheus = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/prometheus ];
+        };
+
+        forgejo = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/forgejo ];
+        };
+
+        runner = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/runner ];
+        };
+
+        vaultwarden = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/vaultwarden ];
+        };
+
+        auth = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/auth ];
+        };
+
+        #
+        # Client Computers
+        #
+
+        mip3 = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs inventory; };
+          modules = modules ++ [ ./hosts/mip3 ];
+        };
+
+        hrattic = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = modules ++ [ ./hosts/hrattic ];
+        };
+
+      };
+
+      packages.${system} = {
+        default = self.packages.${system}.release;
+        release = release-script;
+
+        gw-nbg = self.nixosConfigurations.gw-nbg.config.system.build.diskoImages;
+
+        gw-genesis = self.nixosConfigurations.gw-genesis.config.system.build.diskoImages;
+        resolver = self.nixosConfigurations.resolver.config.system.build.diskoImages;
+        prometheus = self.nixosConfigurations.prometheus.config.system.build.diskoImages;
+        forgejo = self.nixosConfigurations.forgejo.config.system.build.diskoImages;
+        runner = self.nixosConfigurations.runner.config.system.build.diskoImages;
+        vaultwarden = self.nixosConfigurations.vaultwarden.config.system.build.diskoImages;
+        auth = self.nixosConfigurations.auth.config.system.build.diskoImages;
+      };
+
+      apps.${system} = {
+        release = {
+          type = "app";
+          program = "${self.packages.${system}.release}/bin/release";
+        };
       };
     };
-
-    # Map images to packages for easy building
-    packages.${system} = {
-        vm-image = self.nixosConfigurations.generic-guest.config.system.build.images.qcow2;
-        proxmox-lxc = self.nixosConfigurations.generic-guest.config.system.build.images.proxmox-lxc;
-    };
-  };
 }
--- justfile
+++ justfile
@@ -1,25 +1,63 @@
-#!/usr/bin/env just --justfile
-
 # SPDX-FileCopyrightText: 2026 maximizzar <mail@maximizzar.de>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# A list of all available operations
 default:
-  just --list
+    just --list
 
-# Update the complete flake
-update-flake:
-	nix flake update
+# Build a QCow2 Disk-Image
+build-disk-image flake:
+    nix build .#{{ flake }}
 
-# Build generic VM image (QCOW2)
-build-vm:
-    nix build path:.#vm-image
+# Push a QCow2 Disk-Image to a remote
+push-disk-image host remote_path:
+    scp result/main.qcow2 {{ host }}:{{ remote_path }}
 
-# Build Proxmox LXC template
-build-lxc:
-    nix build path:.#proxmox-lxc
+# Deploy a flake target to a remote
+deploy flake host:
+    nix run nixpkgs#nixos-rebuild -- switch --flake .#{{ flake }} --target-host "{{ host }}" --sudo
 
-# Deploy a Forwarding BIND9 NS with Filtering
-deploy-nameserver target:
-	nix run nixpkgs#nixos-rebuild --switch --flake .#nameserver --target-host {{ target }}
+deploy-build-remote flake host:
+    nix run nixpkgs#nixos-rebuild switch -- --flake .#{{ flake }} --target-host "{{ host }}" --build-host "{{ host }}" --sudo
+
+# Deploy a flake to local system
+deploy-local flake:
+    sudo nixos-rebuild switch --flake .#{{ flake }}
+
+# Edit Secrets from a specified host
+edit-secrets host:
+    nix run nixpkgs#sops -- edit hosts/{{ host }}/secrets.yaml
+
+# Get a Hosts age key using ssh-to-age
+get-agekey host:
+    nix-shell -p ssh-to-age --run 'ssh-keyscan {{ host }} | ssh-to-age'
+
+# Updates nvfetcheres _sources and generates a git commit with changes
+nvfetcher:
+    @echo "saving current workspace state..."
+    git stash push -m "temp-stash-for-nvfetcher"
+
+    @echo "Updating nvfetcher sources"
+    nix run nixpkgs#nvfetcher
+
+    @echo "Committing sources"
+    git add _sources
+    git commit -m "chore(nvfetcher): update sources"
+
+    @echo "Restoring workspace..."
+    git stash pop
+
+# Updates Lockfile and generates git commit with changes
+update:
+    @echo "Saving current workspace state..."
+    git stash push -m "temp-stash-for-update"
+
+    @echo "Updating flake.lock..."
+    nix flake update
+
+    @echo "Committing lockfile..."
+    git add flake.lock
+    git commit -m "chore: update flake.lock"
+
+    @echo "Restoring workspace..."
+    git stash pop
